b896b465eec0d6ec3a5091d01a71639e46b94385
CereZ22 <franchitoss050209@gmail.com>
2025-01-06 21:34:30 +0100

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,16 @@
+Cibo = {
+    ox_target = true -- se è false si usa esx textui, se è true si usa ox_target
+}
+
+CereZCibo = {
+    {
+        coords = vector3(44.1376, -998.1729, 29.3360),
+        pedmodel = 's_m_y_chef_01',
+        pedheading = 0
+    },
+    {
+        coords = vector3(-1549.0155, -440.8293, 35.8820),
+        pedmodel = 'mp_m_shopkeep_01',
+        pedheading = 5.0
+    }
+}
